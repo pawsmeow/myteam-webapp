9869fe51c700c9d94f197720072b5d532febf84d
pawsmeow <azamatulymuhamedzan@gmail.com>
2025-07-13 04:33:27 +0500

    Initial commit - MyTeam MLBB form UI

--- assets/index.js
+++
@@ -1,8 +0,0 @@
-import { createElement } from "https://esm.sh/react";
-import { createRoot } from "https://esm.sh/react-dom/client";
-
-const App = () => createElement('h1', {
-  style: { textAlign: 'center', marginTop: '40px', color: '#4F46E5' }
-}, 'Hello from MyTeam MLBB!');
-
-createRoot(document.getElementById('root')).render(createElement(App));
